197824bac5cd6bf7f999a0985755d4c72e075357
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-08-31 11:23:00 -0400

    Server initial files

---
+++ server/src/main/java/diego/bsaletest/BsaleTestApplication.java
@@ -0,0 +1,13 @@
+package diego.bsaletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsaleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsaleTestApplication.class, args);
+	}
+
+}
