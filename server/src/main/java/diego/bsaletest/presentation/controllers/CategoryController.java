d5f72fa4520c041628a00b306a545e9d94af2a8c
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-09-01 16:31:21 -0400

    Added README

--- server/src/main/java/diego/bsaletest/presentation/controllers/CategoryController.java
+++ server/src/main/java/diego/bsaletest/presentation/controllers/CategoryController.java
@@ -6,14 +6,12 @@ import diego.bsaletest.presentation.model.CategoryDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
